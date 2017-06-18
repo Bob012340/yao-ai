63f3c91bf4dc3cd7e870e0e65080aacfb0a2d62e
Bob012340 <lseabrook333@gmail.com>
2017-06-19 00:02:53 +0100

    Tidy up and possibly adding command line arguments

--- predict.py
+++ predict.py
@@ -1,5 +1,4 @@
-import re
-import sys
+import h5py
 import yaoai
 
 import numpy as np
@@ -11,27 +10,31 @@ from keras.layers import LSTM
 from keras.layers import Masking
 
 
-sentence_length = 40
-dataX, dataY, story, word_to_int, int_to_word = yaoai.setup(sentence_length, './input.txt')
+def main():
+    sentence_length = 40
+    dataX, dataY, story, word_to_int, int_to_word = yaoai.setup(sentence_length, './input.txt')
 
-model = Sequential([
-    Masking(mask_value=0.0, input_shape=(dataX.shape[1:])),
-    LSTM(256, return_sequences=True),
-    Dropout(0.2),
-    LSTM(256, return_sequences=True),
-    Dropout(0.2),
-    Dense(dataY.shape[2], activation='softmax'),
-])
-model.load_weights('./model.hdf5')
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+    model = Sequential([
+        Masking(mask_value=0.0, input_shape=(dataX.shape[1:])),
+        LSTM(256, return_sequences=True),
+        Dropout(0.2),
+        LSTM(256, return_sequences=True),
+        Dropout(0.2),
+        Dense(dataY.shape[2], activation='softmax'),
+    ])
+    model.load_weights('./model.hdf5')
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-model_output = model.predict(dataX)
-file_path = './output.txt'
-file = open(file_path, 'w')
-for x in model_output:
-    output_data = []
-    for y in x:
-        output_data.append(int_to_word[np.argmax(y)])
-    output_data.append('\n\n')
-    file.writelines(output_data)
-file.close()
+    model_output = model.predict(dataX)
+    file_path = './output.txt'
+    file = open(file_path, 'w')
+    for x in model_output:
+        output_data = []
+        for y in x:
+            output_data.append(int_to_word[np.argmax(y)])
+        output_data.append('\n\n')
+        file.writelines(output_data)
+    file.close()
+
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+import sys
+import getopt
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -8,26 +11,32 @@ from keras.callbacks import ModelCheckpoint
 from yaoai import setup
 
 
-sentence_length = 40
-dataX, dataY, story, word_to_int, int_to_word = setup(sentence_length)
+def main(argv):
+    sentence_length = 40
+    dataX, dataY, story, word_to_int, int_to_word = setup(sentence_length, './input.txt')
 
-print(dataX)
+    print(dataX)
 
-model = Sequential([
-    Masking(mask_value=0.0, input_shape=(dataX.shape[1:])),
-    LSTM(256, return_sequences=True),
-    Dropout(0.2),
-    LSTM(256, return_sequences=True),
-    Dropout(0.2),
-    Dense(dataY.shape[2], activation='softmax'),
-])
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+    model = Sequential([
+        Masking(mask_value=0.0, input_shape=(dataX.shape[1:])),
+        LSTM(256, return_sequences=True),
+        Dropout(0.2),
+        LSTM(256, return_sequences=True),
+        Dropout(0.2),
+        LSTM(256, return_sequences=True),
+        Dropout(0.2),
+        Dense(dataY.shape[2], activation='softmax'),
+    ])
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-file_path = 'word-LSTM-{epoch:02d}.hdf5'
-checkpoint = ModelCheckpoint(file_path, monitor='loss', verbose=1, save_best_only=True, mode='min')
-callbacks_list = [checkpoint]
+    file_path = 'word-LSTM-{epoch:02d}.hdf5'
+    checkpoint = ModelCheckpoint(file_path, monitor='loss', verbose=1, save_best_only=True, mode='min')
+    callbacks_list = [checkpoint]
 
-model.fit(dataX, dataY, epochs=80, batch_size=sentence_length, callbacks=callbacks_list)
+    model.fit(dataX, dataY, epochs=80, batch_size=sentence_length, callbacks=callbacks_list)
 
-file_path = 'model.hdf5'
-model.save(file_path)
+    file_path = 'model.hdf5'
+    model.save(file_path)
+
+if __name__ == '__main__':
+    main(sys.argv)
